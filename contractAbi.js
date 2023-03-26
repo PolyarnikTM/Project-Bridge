ca922121c09194e928bd5633d2a5823feabab80f
PolyarnikTM <95944262+PolyarnikTM@users.noreply.github.com>
2023-03-26 14:25:52 +0400

    Add files via upload

---
+++ contractAbi.js
@@ -0,0 +1,31 @@
+//ОТ ДРУГОГО ПРОЕКТА
+
+const contractAbi = [ 
+    { 
+     "inputs": [], 
+     "stateMutability": "nonpayable", 
+     "type": "constructor" 
+    }, 
+    { 
+     "inputs": [], 
+     "name": "buy", 
+     "outputs": [], 
+     "stateMutability": "payable", 
+     "type": "function" 
+    }, 
+    { 
+     "inputs": [], 
+     "name": "token", 
+     "outputs": [ 
+      { 
+       "internalType": "contract ABOBA", 
+       "name": "", 
+       "type": "address" 
+      } 
+     ], 
+     "stateMutability": "view", 
+     "type": "function" 
+    } 
+   ] 
+    
+   export default contractAbi
